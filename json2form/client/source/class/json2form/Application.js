ef8df98a09ff4792d3813d863e1199f039860b49
odeimaiz <maiz@itis.swiss>
2019-11-04 11:25:34 +0100

    FakeData added

--- json2form/client/source/class/json2form/Application.js
+++ json2form/client/source/class/json2form/Application.js
@@ -178,89 +178,9 @@ qx.Class.define("json2form.Application", {
     },
 
     __populateWithfake: function() {
-      this.__jsonSchema.setValue(this.__fakeJsonSchema());
-      this.__uiSchema.setValue(this.__fakeUiSchema());
-      this.__formData.setValue(this.__fakeFormData());
-    },
-
-    __fakeJsonSchema: function() {
-      return JSON.stringify({
-        "title": "A registration form",
-        "description": "A simple form example.",
-        "type": "object",
-        "required": [
-          "firstName",
-          "lastName"
-        ],
-        "properties": {
-          "firstName": {
-            "type": "string",
-            "title": "First name",
-            "default": "Chuck"
-          },
-          "lastName": {
-            "type": "string",
-            "title": "Last name"
-          },
-          "age": {
-            "type": "integer",
-            "title": "Age"
-          },
-          "bio": {
-            "type": "string",
-            "title": "Bio"
-          },
-          "password": {
-            "type": "string",
-            "title": "Password",
-            "minLength": 3
-          },
-          "telephone": {
-            "type": "string",
-            "title": "Telephone",
-            "minLength": 10
-          }
-        }
-      }, null, 4);
-    },
-
-    __fakeUiSchema: function() {
-      return JSON.stringify({
-        "firstName": {
-          "ui:autofocus": true,
-          "ui:emptyValue": ""
-        },
-        "age": {
-          "ui:widget": "updown",
-          "ui:title": "Age of person",
-          "ui:description": "(earthian year)"
-        },
-        "bio": {
-          "ui:widget": "textarea"
-        },
-        "password": {
-          "ui:widget": "password",
-          "ui:help": "Hint: Make it strong!"
-        },
-        "date": {
-          "ui:widget": "alt-datetime"
-        },
-        "telephone": {
-          "ui:options": {
-            "inputType": "tel"
-          }
-        }
-      }, null, 4);
-    },
-
-    __fakeFormData: function() {
-      return JSON.stringify({
-        "firstName": "Chuck",
-        "lastName": "Norris",
-        "age": 75,
-        "bio": "Roundhouse kicking asses since 1940",
-        "password": "noneed"
-      }, null, 4);
+      this.__jsonSchema.setValue(json2form.FakeData.fakeJsonSchema());
+      this.__uiSchema.setValue(json2form.FakeData.fakeUiSchema());
+      this.__formData.setValue(json2form.FakeData.fakeFormData());
     }
   }
 });
